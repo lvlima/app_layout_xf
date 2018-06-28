d6a1620fb2de50aec069203d2fdd3b0ce688ed4c
lvlima <luizvictor.al@gmail.com>
2018-06-28 11:03:56 -0300

    Commit do App de teste dos Layouts de tela do Xamarin Forms.

---
+++ app_layout_xf/Layouts/Grid/GridPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace app_layout_xf.Layouts.Grid
+{
+    public partial class GridPage : ContentPage
+    {
+        public GridPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ app_layout_xf/Layouts/Relative/RelativePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace app_layout_xf.Layouts.Relative
+{
+    public partial class RelativePage : ContentPage
+    {
+        public RelativePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
